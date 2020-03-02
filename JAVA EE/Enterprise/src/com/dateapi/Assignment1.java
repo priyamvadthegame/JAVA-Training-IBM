80d8b7e72a1669525206f6073207ee2d6011cce4
priyamvad <priyamvad123@gmail.com>
2020-03-02 15:59:16 +0530

    Date

--- JAVA EE/Enterprise/src/com/dateapi/Assignment1.java
+++ JAVA EE/Enterprise/src/com/dateapi/Assignment1.java
@@ -6,10 +6,13 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Period;
 import java.time.Year;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAmount;
 import java.time.temporal.TemporalField;
+import java.util.Locale;
 
 public class Assignment1 {
 
@@ -52,6 +55,26 @@ public class Assignment1 {
 				System.out.println(period.getDays()+" "+period.getMonths()+" "+period.getYears());
 			}
 		}
-		}
+		//Assignment 4
+		ZoneId zone1=ZoneId.of("Europe/Rome");
+		System.out.println("Time in Rome - "+LocalTime.now(zone1));
+		ZoneId zone2=ZoneId.of("Africa/Cairo");
+		System.out.println("Time in Cairo - "+LocalTime.now(zone2));
+		
+	//Assignment 5
+	DateTimeFormatter format1=DateTimeFormatter.ofPattern("d. MMMM yyyy", Locale.FRENCH);
+	LocalDate date=LocalDate.of(2020,9,13);
+	String formattedDate=date.format(format1);
+	System.out.println(formattedDate);
+	
+	//Assignment 6
+	
+	DateTimeFormatter dtf=DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss");
+	LocalDate date2=LocalDate.of(2020, 3, 10);
+	LocalTime time=LocalTime.of(10, 15, 25);
+	LocalDateTime date1=LocalDateTime.of(date2, time);
+	String  formattedDate1= date1.format(dtf);
+	System.out.println(formattedDate1);
+}
 
 }
