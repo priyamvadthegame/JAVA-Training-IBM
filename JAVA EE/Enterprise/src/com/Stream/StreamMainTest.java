0c3302a582429485be6f64ab43d3d9f63f207fd7
priyamvad <priyamvad123@gmail.com>
2020-03-03 16:55:25 +0530

    Stream till assignment 12

--- JAVA EE/Enterprise/src/com/Stream/StreamMainTest.java
+++ JAVA EE/Enterprise/src/com/Stream/StreamMainTest.java
@@ -2,7 +2,11 @@ package com.Stream;
 
 import java.util.Comparator;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 public class StreamMainTest {
 
 	public static void main(String[] args) {
@@ -20,7 +24,29 @@ public class StreamMainTest {
 		System.out.println(TestData.getAllNews().stream().filter((News n)->n.getComment().contains("budget")).count());
 		System.out.println("\n**Assignment6 answer\n");
 		TestData.getAllNews().stream().collect(Collectors.groupingBy(News::getCommentByUser,Collectors.counting())).forEach((key, value) -> System.out.println(key + ":" + value));
-	
+		System.out.println("\n**Assignment7 answer\n");
+		TestData.getAllTransactions().stream().filter((Transaction t)->t.getYear()==2011).sorted(Comparator.comparing(Transaction::getValue)).forEach(System.out::println);
+		System.out.println("\n**Assignment8 answer\n");
+		TestData.getAllTransactions().stream().map((Transaction t)->t.getTrader().getCity()).distinct().forEach(System.out::println);
+		System.out.println("\n**Assignment9 answer\n");
+		Function<Transaction, String> f=(Transaction t)->t.getTrader().getName();
+		TestData.getAllTransactions().stream().filter((Transaction t)->t.getTrader().getCity().equalsIgnoreCase("Pune")).sorted(Comparator.comparing(f)).forEach(System.out::println);
+		System.out.println("\n**Assignment10 answer\n");
+		System.out.println(TestData.getAllTransactions().stream().sorted(Comparator.comparing(f)).map((Transaction t)->t.getTrader().getName()).collect(joining(",")));
+		System.out.println("\n**Assignment11 answer\n");
+		Optional<Transaction> opTransaction = TestData.getAllTransactions().stream() .filter(t -> t.getTrader().getCity().equalsIgnoreCase("Pune")) .findAny();
+		if(opTransaction.isPresent())
+		{	
+			System.out.println(opTransaction.get().getTrader().getName());
+		}
+		else
+		{
+			System.out.println("No traders from specified city");
+		}
+		
+		System.out.println("\n**Assignment12 answer\n");
+		TestData.getAllTransactions().stream().filter((Transaction t)->t.getTrader().getCity().equalsIgnoreCase("Pune")).map((Transaction t)->t.getValue()).forEach(System.out::println);
+		
 		
 	}
 
